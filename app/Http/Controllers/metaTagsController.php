b95f657d04821d50106c31957ec65b8ef079bcd8
Bienvenido <bienvenidoborico@gmail.com>
2020-04-17 18:10:59 +0000

    agregado metodo store en metaTagsController

--- app/Http/Controllers/metaTagsController.php
+++ app/Http/Controllers/metaTagsController.php
@@ -17,15 +17,6 @@ class metaTagsController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -33,9 +24,31 @@ class metaTagsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(array $data)
     {
-        //
+        //validar los datos a insertar en la metaTags
+        $validator = Validator::make($data, [
+            'name' => ['required','string','max:25'],
+            'value' => ['required','string','max:50'],
+            'type' => ['required','string',Rule::in(['post','tag','category','author'])],
+            'id_owner' => ['required','string','max:20'],
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('post/create')
+                        ->withErrors($validator)
+                        ->withInput();
+        }
+
+        return metaTags::create([
+                'name' => $data['name'],
+                'value'=>$data['value'],
+                'type'=>$data['type'],
+                'id_owner'=>$data['id_owner']
+            ]);
+
+
+
     }
 
     /**
--- app/metaTags.php
+++ app/metaTags.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class metaTags extends Model
 {
+    protected $table="metaTags";
+
     protected $fillable = [
         'name','value','type','id_user','id_post',
         'id_category','id_tag'
