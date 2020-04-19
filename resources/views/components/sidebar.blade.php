1b3237602dcfcfd11341ae1626b77e04268ae6a2
Bienvenido <bienvenidoborico@gmail.com>
2020-04-19 18:30:24 +0000

    creado componente navbar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,201 +1,42 @@
-<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
-<!------ Include the above in your HEAD tag ---------->
-
-
-        <!--===================
-        Header
-        =======================-->
-
-        <header class="header">
-            <nav class="navbar navbar-toggleable-md navbar-light pt-0 pb-0 ">
-              <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-              </button>
-              <a class="navbar-brand p-0 mr-5" href="#"><img src="http://via.placeholder.com/61x14"></a>
-              <div class="float-left"> <a href="#" class="button-left"><span class="fa fa-fw fa-bars "></span></a> </div>
-              <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                  <li class="nav-item dropdown messages-menu">
-                    <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fa fa-bell-o"></i>
-                      <span class="label label-success bg-success">10</span>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <ul class="dropdown-menu-over list-unstyled">
-                        <li class="header-ul text-center">You have 4 messages</li>
-                        <li>
-                          <!-- inner menu: contains the actual data -->
-                          <ul class="menu list-unstyled">
-                            <li><!-- start message -->
-                            <a href="#">
-                              <div class="pull-left">
-                                <img src="http://via.placeholder.com/160x160" class="rounded-circle  " alt="User Image">
-                              </div>
-                              <h4>
-                              Support Team
-                              <small><i class="fa fa-clock-o"></i> 5 mins</small>
-                              </h4>
-                              <p>Why not buy a new awesome theme?</p>
-                            </a>
-                          </li>
-                          <!-- end message -->
-                          <li>
-                            <a href="#">
-                              <div class="pull-left">
-                                <img src="http://via.placeholder.com/160x160" class="rounded-circle " alt="User Image">
-                              </div>
-                              <h4>
-                              AdminLTE Design Team
-                              <small><i class="fa fa-clock-o"></i> 2 hours</small>
-                              </h4>
-                              <p>Why not buy a new awesome theme?</p>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <div class="pull-left">
-                                <img src="http://via.placeholder.com/160x160" class="rounded-circle " alt="User Image">
-                              </div>
-                              <h4>
-                              Developers
-                              <small><i class="fa fa-clock-o"></i> Today</small>
-                              </h4>
-                              <p>Why not buy a new awesome theme?</p>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <div class="pull-left">
-                                <img src="http://via.placeholder.com/160x160" class="rounded-circle " alt="User Image">
-                              </div>
-                              <h4>
-                              Sales Department
-                              <small><i class="fa fa-clock-o"></i> Yesterday</small>
-                              </h4>
-                              <p>Why not buy a new awesome theme?</p>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <div class="pull-left">
-                                <img src="http://via.placeholder.com/160x160" class="rounded-circle " alt="User Image">
-                              </div>
-                              <h4>
-                              Reviewers
-                              <small><i class="fa fa-clock-o"></i> 2 days</small>
-                              </h4>
-                              <p>Why not buy a new awesome theme?</p>
-                            </a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li class="footer-ul text-center"><a href="#">See All Messages</a></li>
-                    </ul>
-                  </div>
-                </li>
-                <li class="nav-item dropdown notifications-menu">
-                  <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fa fa-envelope-o"></i>
-                    <span class="label label-warning bg-warning">10</span>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <ul class="dropdown-menu-over list-unstyled">
-                      <li class="header-ul text-center">You have 10 notifications</li>
-                      <li>
-                        <!-- inner menu: contains the actual data -->
-                        <ul class="menu list-unstyled">
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                              page and may cause design problems
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-users text-red"></i> 5 new members joined
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                            </a>
-                          </li>
-                          <li>
-                            <a href="#">
-                              <i class="fa fa-user text-red"></i> You changed your username
-                            </a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li class="footer-ul text-center"><a href="#">View all</a></li>
-                    </ul>
-                  </div>
-                </li>
-
-                <li class="nav-item dropdown  user-menu">
-                  <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <img src="http://via.placeholder.com/160x160" class="user-image" alt="User Image" >
-                    <span class="hidden-xs">bootstrap develop</span>
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">bootstrap develop</a>
-                    <a class="dropdown-item" href="#">editar perfil</a>
-                    <a class="dropdown-item" href="#">salir</a>
-                  </div>
-                </li>
-              </ul>
+<aside>
+    <div class="sidebar left ">
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ auth()->user()->profile_img }}" class="rounded-circle" alt="User Image">
             </div>
-          </nav>
-        </header>
-        <div class="main">
-          <aside>
-            <div class="sidebar left ">
-              <div class="user-panel">
-                <div class="pull-left image">
-                  <img src="http://via.placeholder.com/160x160" class="rounded-circle" alt="User Image">
-                </div>
-                <div class="pull-left info">
-                  <p>bootstrap develop</p>
-                  <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-                </div>
-              </div>
-              <ul class="list-sidebar bg-defoult">
-                <li>
-                    <a href="#">
-                        <i class="fa fa-diamond"></i> <span class="nav-label"> Visitar Sitio </span>
-                    </a>
-              </li>
-              <li>
-                  <a href="#"> <i class="fa fa-th-large"></i> <span class="nav-label">Tablero</span></a>
-              </li>
-              <li>
-                  <a href="#" >
-                      <i class="fa fa-bar-chart-o"></i> <span class="nav-label">Entradas</span>
-                 </a>
+            <div class="pull-left info">
+                <p>{{ auth()->user()->name }}</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <ul class="list-sidebar bg-defoult">
+            <li>
+                <a href="#">
+                    <i class="fa fa-diamond"></i> <span class="nav-label"> Visitar Sitio </span>
+                </a>
+            </li>
+            <li>
+                <a href="#"> <i class="fa fa-th-large"></i> <span class="nav-label">Tablero</span></a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-bar-chart-o"></i> <span class="nav-label">Entradas</span>
+                </a>
             </li>
             <li>
                 <a href="#"><i class="fa fa-laptop"></i> <span class="nav-label">Autores</span></a>
             </li>
             <li>
-                <a href="#" data-toggle="collapse" data-target="#settings" class="collapsed active" >
+                <a href="#" data-toggle="collapse" data-target="#settings" class="collapsed active">
                     <i class="fa fa-table"></i> <span class="nav-label">Ajustes</span>
                     <span class="fa fa-chevron-left pull-right"></span>
                 </a>
-            <ul  class="sub-menu collapse" id="settings" >
-              <li><a href=""> Pagina</a></li>
-              <li><a href=""> Etiquetas</a></li>
-              <li><a href=""> Categorias</a></li>
-            </ul>
-          </li>
-      </ul>
-      </div>
-      </aside>
-      </div>
-
+                <ul class="sub-menu collapse" id="settings">
+                    <li><a href=""> Pagina</a></li>
+                    <li><a href=""> Etiquetas</a></li>
+                    <li><a href=""> Categorias</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</aside>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<x-navbar />
 <x-sidebar />
-
+<h2>hola</h2>
 @endsection
