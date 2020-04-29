3bc36fbcf9d0d6732a9609c5f712b4cb7a9e6219
bienvenidoboriko <bienvenidoborico@gmail.com>
2020-04-29 12:12:25 +0000

    actualizado DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(RolSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(SettingSeeder::class);
     }
 }
