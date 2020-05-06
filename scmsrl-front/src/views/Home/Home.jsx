65ce0ad9c098eb0c617dce885089329cfc99fdc4
bienvenidoboriko <bienvenidoborico@gmail.com>
2020-05-06 09:44:59 +0000

    cargados datos en vista home

--- scmsrl-front/src/views/Home/Home.jsx
+++ scmsrl-front/src/views/Home/Home.jsx
@@ -1,7 +1,33 @@
-import React, { Fragment } from "react";
-import { getPosts } from "./../../utils/peticiones";
+import React, { Fragment, useState, useEffect } from "react";
+import { getPosts, getCategories, getSettings, getTags } from "./../../utils/peticiones";
 import NavBar from "./../../components/NavBar/NavBar";
 const Home = () => {
+  const [data, setData] = useState({ posts: "", tags: "", categories: "", settings: "" });
+
+  const getData = async () => {
+    let datas = {};
+    datas.posts = await getPosts()
+      .then((res) => res.data)
+      .then((data) => data.posts);
+
+    datas.tags = await getTags()
+      .then((res) => res.data)
+      .then((data) => data.tags);
+
+    datas.categories = await getCategories()
+      .then((res) => res.data)
+      .then((data) => data.categories);
+
+    datas.settings = await getSettings()
+      .then((res) => res.data)
+      .then((data) => data.settings);
+    return datas;
+  };
+
+  useEffect(async () => {
+    setData(await getData());
+  }, []);
+
   return (
     <Fragment>
       <NavBar />
@@ -10,9 +36,4 @@ const Home = () => {
   );
 };
 
-{
-  getPosts()
-    .then((res) => res.data)
-    .then((data) => console.log(data));
-}
 export default Home;
