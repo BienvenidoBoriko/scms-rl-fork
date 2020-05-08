07427555b0007d973cfcd2699559f60505eeffed
bienvenidoboriko <bienvenidoborico@gmail.com>
2020-05-08 09:22:32 +0000

    agregado componente PostHeader

---
+++ scmsrl-front/src/components/post-header/post-header.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+
+const PostHeader = ({ title, desc, author, created_at, img }) => {
+  return (
+    <header class="masthead" style={{ "background-image": `url(${img})` }}>
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-lg-8 mx-auto">
+            <div class="post-heading">
+              <h1>{title}</h1>
+              <h2 class="subheading">{desc}</h2>
+              <span class="meta">
+                Publicado por<a href="#">{author}</a>&nbsp;el dia {created_at}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PostHeader;
--- scmsrl-front/src/views/Post/Post.jsx
+++ scmsrl-front/src/views/Post/Post.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom";
 import NavBar from "./../../components/NavBar/NavBar";
+import PostHeader from "./../../components/post-header/post-header";
 const Post = (props) => {
   let { id } = useParams();
   return (
     <Fragment>
       <NavBar />
+      <PostHeader />
       <div>estamos en post {id}</div>
     </Fragment>
   );
